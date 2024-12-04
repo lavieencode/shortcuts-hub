3724c4cc22782875e71cb72d3b7fed3eaea406c6
Nicole Archambault <nicole@debotchery.ai>
2024-12-04 06:03:53 +0000

    feat: Set default filter values to published and not deleted
    Change default page load settings for shortcuts filtering:
    - Set status filter default to 'published'
    - Set deleted filter default to 'not deleted'
    - Update reset button to use same defaults

--- assets/js/shortcuts/shortcuts-filters.js
+++ assets/js/shortcuts/shortcuts-filters.js
@@ -3,6 +3,10 @@ jQuery(document).ready(function() {
 });
 
 function initializeFilters() {
+    // Set default values
+    jQuery('#filter-status').val('publish');
+    jQuery('#filter-deleted').val('false');
+    
     // Handle filter changes
     jQuery('#filter-status, #filter-deleted').on('change', function() {
         console.log('Filter changed:', {
@@ -23,7 +27,8 @@ function initializeFilters() {
 
     // Handle reset button
     jQuery('#reset-filters').on('click', function() {
-        jQuery('#filter-status, #filter-deleted').val('');
+        jQuery('#filter-status').val('publish');
+        jQuery('#filter-deleted').val('false');
         jQuery('#search-input').val('');
         fetchShortcuts();
     });
